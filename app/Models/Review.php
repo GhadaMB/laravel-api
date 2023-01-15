76cc376e829e3f50496474ef4e801b5b350c5b0a
ghada <ghadab@istansa.com>
2023-01-15 18:39:45 +0300

    relations & fix seeder

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace App\Models\Model;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function product() {
+
+        return $this->belongsTo(Product::class);
+
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Product;
+use App\Models\Review;
+
 
 use Illuminate\Database\Seeder;
 //use App\Models\Product;
@@ -15,9 +18,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$product = Product::factory(50)->create();
-        \App\Models\Product::factory()->count(50)->create();
-        //$review = Review::factory(100)->create();
-        \App\Models\Review::factory()->count(100)->create();
+        $product = Product::factory(50)->create();
+        //\App\Models\Product::factory()->count(50)->create();
+        $review = Review::factory(100)->create();
+        //\App\Models\Review::factory()->count(100)->create();
     }
 }
