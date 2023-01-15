3ec19c381dd03f979e9d4fd97af9d2b98b49c0cd
ghada <ghadab@istansa.com>
2023-01-15 11:37:29 +0300

    seed & factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+//use App\Models\Product;
+//use App\Models\Review;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //$product = Product::factory(50)->create();
+        \App\Models\Product::factory()->count(50)->create();
+        //$review = Review::factory(100)->create();
+        \App\Models\Review::factory()->count(100)->create();
     }
 }
