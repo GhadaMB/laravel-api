6f948f6e9d29ddf037ab0da62729ff99c058e69d
ghada <ghadab@istansa.com>
2023-01-16 11:25:54 +0300

    Product Collection Resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -13,7 +13,18 @@ class ProductCollection extends ResourceCollection
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-    {
-        return parent::toArray($request);
+    {  
+        return [
+
+            'name' => $this->name,
+            'totalPrice' => round( ( 1 - ($this->discount/100) ) * $this->price ,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Rating yet',
+            'discount' => $this->discount,
+            'href' => [
+                'link' => route('products.show', $this->id),
+            ]
+
+
+        ];
     }
 }
